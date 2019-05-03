4f20506d9056d70c2ed6b42080cb8f6cbc0f0f39
Ben Groseclose <benjamin.groseclose@hotmail.com>
2019-05-02 22:46:53 -0400

    login process and start home process

--- android/BudgetTracker/app/src/main/java/com/tracker/budget/budgettracker/Activites/LoginActivity.kt
+++ android/BudgetTracker/app/src/main/java/com/tracker/budget/budgettracker/Activites/LoginActivity.kt
@@ -5,14 +5,13 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.design.widget.NavigationView
+import android.support.v4.app.Fragment
 import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.widget.Toolbar
+import com.tracker.budget.budgettracker.LoginFragment
 import com.tracker.budget.budgettracker.R
+import com.tracker.budget.budgettracker.SettingsFragment
 import kotlinx.android.synthetic.main.activity_login.*
-import android.support.annotation.NonNull
-import android.R
-import android.view.MenuItem
-
 
 fun Context.openLoginActivity() : Intent {
     return Intent()
@@ -20,12 +19,13 @@ fun Context.openLoginActivity() : Intent {
 
 class LoginActivity : AppCompatActivity() {
 
+    var existingFragment: Fragment? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-        var toolbar: Toolbar
-        toolbar = findViewById(R.id.login_toolbar)
+        var toolbar: Toolbar = findViewById(R.id.home_toolbar)
         setSupportActionBar(toolbar)
         supportActionBar?.setDisplayShowTitleEnabled(false)
 
@@ -35,13 +35,33 @@ class LoginActivity : AppCompatActivity() {
             login_drawer_layout, toolbar, R.string.drawer_open, R.string.drawer_close)
         login_drawer_layout.addDrawerListener(drawerToggle)
         drawerToggle.syncState()
-
+        setupDrawerContent(login_drawer)
+        initLoginFragment()
     }
+
+    private fun initLoginFragment() {
+        if (login_container != null) {
+            val fragmentTransaction = supportFragmentManager.beginTransaction()
+            fragmentTransaction.add(R.id.login_container, LoginFragment())
+            fragmentTransaction.commit()
+            existingFragment = LoginFragment()
+        }
+        title = getString(R.string.login)
+    }
+
 
     private fun setupDrawerContent(navigationView: NavigationView) {
         navigationView.setNavigationItemSelectedListener {
-            //presenter.drawerItemSelected(menuItem.getItemId())
+            if(it.itemId == R.id.login_menu) {
+                supportFragmentManager.beginTransaction().replace(R.id.login_container, LoginFragment()).addToBackStack(null).commit()
+            } else if(it.itemId == R.id.settings_menu) {
+                supportFragmentManager.beginTransaction().replace(R.id.login_container, SettingsFragment()).addToBackStack(null).commit()
+            }
+
+            title = it.title
+            login_drawer_layout.closeDrawers()
             true
         }
     }
+
 }
---
+++ android/BudgetTracker/app/src/main/java/com/tracker/budget/budgettracker/SettingsFragment.kt
@@ -0,0 +1,7 @@
+package com.tracker.budget.budgettracker
+
+import android.support.v4.app.Fragment
+
+class SettingsFragment: Fragment() {
+
+}
