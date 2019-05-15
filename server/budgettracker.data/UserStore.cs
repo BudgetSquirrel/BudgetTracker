131613ad44b74a2db273eaebaa6b3631799aa4bd
Ian Kirkpatrick <ianmann56@gmail.com>
2019-05-14 21:48:26 -0400

    Create UserStore and hook it up with Identity

---
+++ server/budgettracker.data/UserStore.cs
@@ -0,0 +1,46 @@
+using budgettracker.common;
+using budgettracker.data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace budgettracker.data
+{
+    /// <summary>
+    /// Contains logic to perform CRUD operations on users. This acts as a
+    /// translation between the basic <see cref="User" /> class and the
+    /// data model so that the business layer doesn't have to worry about
+    /// the data storage implementation.
+    /// </summary>
+    public class UserStore
+    {
+        private UserManager<UserModel> _userManager;
+
+        public UserStore(IServiceProvider serviceProvider)
+        {
+            _userManager = (UserManager<UserModel>) serviceProvider.GetService(typeof(UserManager<UserModel>));
+        }
+
+        /// <summary>
+        /// <p>
+        /// Creates the user and returns whether or not it was created.
+        /// </p>
+        /// <p>
+        /// If an error occurs, the errors list will be populated with the
+        /// error codes for the errors which occurred.
+        /// </p>
+        /// </summary>
+        public bool Register(User userData, out IEnumerable<string> errors)
+        {
+            UserModel user = new UserModel {
+                UserName = userData.UserName,
+                Email = userData.Email
+            };
+            IdentityResult result = _userManager.CreateAsync(user, userData.Password).Result;
+            errors = result.Errors.Select(idError => idError.Code); // Codes may include "DuplicateUserName".
+            return result.Succeeded;
+        }
+    }
+}
--- server/budgettracker.web/Controllers/HomeController.cs
+++ server/budgettracker.web/Controllers/HomeController.cs
@@ -1,8 +1,13 @@
-﻿using System;
+﻿using budgettracker.common;
+using budgettracker.data;
+using budgettracker.data.Models;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using budgettracker.web.Models;
 
@@ -12,6 +17,23 @@ namespace budgettracker.web.Controllers
     {
         public IActionResult Index()
         {
+            User user = new User {
+                Email = "ianmann56@gmail.com",
+                UserName = "ianmann56",
+                Password = "Saline!54"
+            };
+            IEnumerable<string> userCreateErrors;
+            // UserModel user = new UserModel { UserName = Input.UserName, Email = Input.Email };
+            // var result = await _userManager.CreateAsync(user, Input.Password);
+            UserStore userStore = new UserStore(HttpContext.RequestServices);
+            Console.WriteLine(userStore);
+            bool userCreated = userStore.Register(user, out userCreateErrors);
+            foreach (string code in userCreateErrors)
+            {
+                Console.WriteLine("Here: " + code);
+            }
+            Console.WriteLine(userCreated);
+            ViewData["Test"] = "Hello world!";
             return View();
         }
 
