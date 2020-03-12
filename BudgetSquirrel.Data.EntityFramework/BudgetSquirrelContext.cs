88d4afd6a263722fd4fb18797947509aa34c11c4
Ian Kirkpatrick <ianmann56@gmail.com>
2020-03-12 18:29:26 -0400

    Add back data layer

---
+++ BudgetSquirrel.Data.EntityFramework/BudgetSquirrelContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using BudgetSquirrel.Business.Auth;
+using BudgetSquirrel.Business.BudgetPlanning;
+using BudgetSquirrel.Business.Tracking;
+using Microsoft.EntityFrameworkCore;
+
+namespace BudgetSquirrel.Data.EntityFramework
+{
+    public class BudgetSquirrelContext : DbContext
+    {
+        public DbSet<Budget> Budgets { get; set; }
+        public DbSet<DaySpanDuration> DaySpanDurations { get; set; }
+        public DbSet<MonthlyBookEndedDuration> MonthlyBookEndedDurations { get; set; }
+        public DbSet<BudgetPeriod> BudgetPeriods { get; set; }
+        public DbSet<Transaction> Transactions { get; set; }
+        public DbSet<User> Users { get; set; }
+    }
+}
