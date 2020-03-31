0c2246c6dafdb032751b2634102cca2e356e6bee
Ian Kirkpatrick <ikirkpat@oaisd.org>
2020-03-31 19:26:13 -0400

    Cleanup data layer and add user model

--- BudgetSquirrel.Data.EntityFramework/BudgetSquirrelContext.cs
+++ BudgetSquirrel.Data.EntityFramework/BudgetSquirrelContext.cs
@@ -1,18 +1,10 @@
-﻿using System;
-using BudgetSquirrel.Business.Auth;
-using BudgetSquirrel.Business.BudgetPlanning;
-using BudgetSquirrel.Business.Tracking;
+﻿using BudgetSqurrel.Data.EntityFramework.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace BudgetSquirrel.Data.EntityFramework
 {
     public class BudgetSquirrelContext : DbContext
     {
-        public DbSet<Budget> Budgets { get; set; }
-        public DbSet<DaySpanDuration> DaySpanDurations { get; set; }
-        public DbSet<MonthlyBookEndedDuration> MonthlyBookEndedDurations { get; set; }
-        public DbSet<BudgetPeriod> BudgetPeriods { get; set; }
-        public DbSet<Transaction> Transactions { get; set; }
-        public DbSet<User> Users { get; set; }
+        public DbSet<UserRecord> Users { get; set; }
     }
 }
---
+++ BudgetSquirrel.Data.EntityFramework/Models/UserRecord.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace BudgetSqurrel.Data.EntityFramework.Models
+{
+  public class UserRecord
+  {
+    public Guid Id { get; set; }
+    public string Username { get; set; }
+    public string Password { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Email { get; set; }
+  }
+}
